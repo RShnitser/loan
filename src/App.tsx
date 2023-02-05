c71629dde6329578b9b5ce93d899aa2015b42543
Roman Shnitser <romanshnitser@hotmail.com>
2023-02-04 20:55:16 -0500

    add nav and title css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoanApp from './components/LoanApp/LoanApp';
+import './App.css';
 
 export function App(): JSX.Element {
   return <LoanApp />;
--- src/components/LoanApp/LoanApp.tsx
+++ src/components/LoanApp/LoanApp.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
+import houseIMG from '../../assets/icons/buy-a-house.svg';
+import './LoanApp.css';
 
 const loanApp = (): JSX.Element => {
   return (
-    <>
-      <nav></nav>
-      <h2>Let&apos;s plan your saving goal</h2>
-      <div>
+    <div className="page-container">
+      <nav className="nav-container">
+        <div className="logo">origin</div>
+      </nav>
+      <div className="title">
+        Let&apos;s plan your <span className="bold">saving goal</span>
+      </div>
+      <div className="card-container">
         <form>
-          <div>Buy a house</div>
-          <div>Saving goal</div>
-          <label htmlFor="amount"></label>
-          <input type="number" />
-          <label htmlFor="month"></label>
-          <input type="number" />
-          <div>Monthly amount</div>
+          <div className="goal-container">
+            <div className="loan-image">
+              <img src={houseIMG} alt="goal" />
+            </div>
+            <div className="goal-text">
+              <div className="goal-title">Buy a house</div>
+              <div className="goal-subtitle">Saving goal</div>
+            </div>
+          </div>
+          <div className="input-container">
+            <div>
+              <div className="input-label">Total amount</div>
+              <input className="input-border" type="text" />
+            </div>
+            <div>
+              <div>Reach goal by</div>
+              <div>
+                <input type="button" value="<" />
+                <div>
+                  <div>year</div>
+                  <div>month</div>
+                </div>
+                <input type="button" value=">" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>Monthly amount</div>
+            <div>amount</div>
+          </div>
           <div>planning</div>
           <button type="submit">Confirm</button>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
