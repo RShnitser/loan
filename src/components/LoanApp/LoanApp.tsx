5b731052b7b1968688cf107bb704de8073006145
Roman Shnitser <romanshnitser@hotmail.com>
2023-02-05 23:23:20 -0500

    complete styles and format currency

--- src/components/LoanApp/LoanApp.tsx
+++ src/components/LoanApp/LoanApp.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import houseIMG from '../../assets/icons/buy-a-house.svg';
 import './LoanApp.css';
 
-const loanApp = (): JSX.Element => {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const LoanApp = (): JSX.Element => {
+  const [amount, setAmount] = useState(0);
+  const [reachDate, setReachDate] = useState(0);
+
+  const usdFormatter = new Intl.NumberFormat('en-us', {
+    style: 'decimal',
+    //currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
   return (
     <div className="page-container">
       <nav className="nav-container">
         <div className="logo">origin</div>
       </nav>
       <div className="title">
-        Let&apos;s plan your <span className="bold">saving goal</span>
+        Let&apos;s plan your <span>saving goal</span>
       </div>
       <div className="card-container">
         <form>
@@ -25,30 +36,63 @@ const loanApp = (): JSX.Element => {
           <div className="input-container">
             <div>
               <div className="input-label">Total amount</div>
-              <input className="input-border" type="text" />
+              <div className="border flex-container">
+                <div className="currency">$</div>
+                <input
+                  className="input-amount"
+                  type="text"
+                  value={usdFormatter.format(amount / 100)}
+                  onChange={({
+                    target: { value },
+                  }: React.ChangeEvent<HTMLInputElement>) => {
+                    const result = value.replace(/\D/g, '');
+                    setAmount(parseFloat(result));
+                  }}
+                />
+              </div>
             </div>
             <div>
-              <div>Reach goal by</div>
-              <div>
-                <input type="button" value="<" />
-                <div>
-                  <div>year</div>
-                  <div>month</div>
+              <div className="input-label">Reach goal by</div>
+              <div className="border flex-container">
+                <input
+                  type="button"
+                  value="&lsaquo;"
+                  onClick={() => {
+                    setReachDate(reachDate - 1);
+                  }}
+                />
+                <div className="center-text">
+                  <div className="bold">month</div>
+                  <div className="input-label">year</div>
                 </div>
-                <input type="button" value=">" />
+                <input
+                  type="button"
+                  value="&rsaquo;"
+                  onClick={() => {
+                    setReachDate(reachDate + 1);
+                  }}
+                />
               </div>
             </div>
           </div>
-          <div>
-            <div>Monthly amount</div>
-            <div>amount</div>
+          <div className="border amount-container">
+            <div className="monthly-label">
+              <div>Monthly amount</div>
+              <div className="amount">amount</div>
+            </div>
+            <div className="center-text planning">
+              You&apos;re planning <span>{reachDate} monthly deposits</span> to
+              reach your <span>${usdFormatter.format(amount / 100)}</span> goal
+              by <span>some date</span>
+            </div>
           </div>
-          <div>planning</div>
-          <button type="submit">Confirm</button>
+          <button className="btn-confirm" type="submit">
+            Confirm
+          </button>
         </form>
       </div>
     </div>
   );
 };
 
-export default loanApp;
+export default LoanApp;
